dbdbce5826c208893a915513c9b61ac0ef7794c4
ANTCZAK Michael <mantczak@ed.ac.uk>
2016-04-13 13:13:27 +0100

    END OF DAY COMMIT and update

--- js/EUL_SCAN_ANNEXE.js
+++ js/EUL_SCAN_ANNEXE.js
@@ -17,5 +17,15 @@ if ($("#openRSRequest1")) {
 	$("#openRSRequest1").css('background-color', 'pink');
 }
  
+jQuery.PRIMO.records.each(
+        function(index){ 
+		// the following gives the location of the item - which library, it doesnt 
+		// exist for electronic
+		// the trouble is to get to the iframe
+            if (jQuery.PRIMO.records[index].getData().display.availlibrary) {
+                $("#openRSRequest1").css('background-color', 'pink');
+            } 
+        }
+      ); 
  
 });
